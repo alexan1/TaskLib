cd631b6bf0f8e39e2d0e5dfc22f8df8290b75df0
Alexandre Konioukhov <alexan1@gmail.com>
2019-10-18 14:05:09 -0400

    numbers

---
+++ TaskLib/Numbers.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskLib
+{
+    public static class Numbers
+    {
+
+        public static bool IsPrime(int number)
+        {
+            if (number <= 1) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+
+            var boundary = (int)Math.Floor(Math.Sqrt(number));
+
+            for (int i = 3; i <= boundary; i += 2)
+                if (number % i == 0)
+                    return false;
+
+            return true;
+        }
+
+        public static IEnumerable<int> Get1000Primes()
+        {
+            var result = new List<int>();            
+
+            for( int i = 2, count = 0; count < 1000; i++)
+            {
+                if (IsPrime(i))
+                {
+                    result.Add(i);
+                    count++;
+                }
+            }
+
+
+            return result;
+        }
+
+    }
+}
---
+++ UnitTestLib/NumbersTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TaskLib;
+
+namespace UnitTestLib
+{
+    [TestClass]
+    public class NumbersTests
+    {
+        [TestMethod]
+        public void Is1000Numbers()
+        {
+            var expected = 1000;
+            var result = Numbers.Get1000Primes();
+
+            Assert.AreEqual(expected, result.Count());
+        }
+
+        [TestMethod]
+        public void ThisIsPrime()
+        {
+            var prime = 3;
+            Assert.IsTrue(Numbers.IsPrime(prime));
+        }
+
+        [TestMethod]
+        public void ThisIsNotPrime()
+        {
+            var notprime = 4;
+            Assert.IsFalse(Numbers.IsPrime(notprime));
+        }
+
+        [TestMethod]
+        public void RandomIsPrime()
+        {
+            var result = Numbers.Get1000Primes();
+            var rand = new Random();
+            int index = rand.Next(2, result.Count());
+            var num =  result.ElementAt(index);
+
+            Assert.IsTrue(Numbers.IsPrime(num));
+        }
+
+
+    }
+}
