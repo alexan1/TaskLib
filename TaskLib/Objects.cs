0ae70f1a6e6bccb3e291ed72c5bce66aafa55731
Alexandre Konioukhov <alexan1@gmail.com>
2019-10-18 20:57:34 -0400

    objects

---
+++ TaskLib/Objects.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskLib
+{
+    public class A
+    {
+        public virtual string F()
+        {
+            return "A.F";
+        }
+    }
+
+    public class B : A
+    {
+        public override string F()
+        {
+            return "B.F";
+        }
+    }
+}
---
+++ UnitTestLib/ObjectsTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TaskLib;
+using System.Diagnostics;
+using System.Reflection;
+
+
+namespace UnitTestLib
+{
+    [TestClass]
+    public class ObjectsTests
+    {
+        [TestMethod]
+        public void ATest()
+        {
+            //var st = new StackTrace();
+            //var sf = st.GetFrame(1);
+
+            //var currentMethodName = sf.GetMethod().Name;
+
+            //var res = MethodBase.GetCurrentMethod().Name;
+
+
+            var expected = "A.F";
+            A a = new A();
+            //var name = type
+            var result = a.F();
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void BTest()
+        {
+            var expected = "B.F";
+            B b = new B();
+            var result = b.F();
+
+            Assert.AreEqual(expected, result);
+        }
+
+
+        [TestMethod]
+        public void ABTest()
+        {
+            var expected = "B.F";
+            A b = new B();
+            var result = b.F();
+
+            Assert.AreEqual(expected, result);
+        } 
+
+    }
+}
