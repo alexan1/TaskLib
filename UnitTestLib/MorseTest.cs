d806501b9a928f58892c0c0776e2b65a2fafa8b6
Alexandre Konioukhov <alexan1@gmail.com>
2019-11-10 17:09:57 -0500

    CollectionAssert instead assert

--- UnitTestLib/MorseTest.cs
+++ UnitTestLib/MorseTest.cs
@@ -13,15 +13,15 @@ namespace UnitTestLib
         [TestMethod]
         public void ASingleSignal()
         {
-            Assert.AreEqual(new List<string> { "E" }, Morse.Possibilities("."));
-            Assert.AreEqual(new List<string> { "A" }, Morse.Possibilities(".-"));
+            CollectionAssert.AreEqual(new List<string> { "E" }, Morse.Possibilities("."));
+            CollectionAssert.AreEqual(new List<string> { "A" }, Morse.Possibilities(".-"));
         }
         [TestMethod]
         public void AWordWithASingleUnknownSignal()
         {
-            Assert.AreEqual(new List<string> { "E", "T" }, Morse.Possibilities("?"));
-            Assert.AreEqual(new List<string> { "I", "N" }, Morse.Possibilities("?."));
-            Assert.AreEqual(new List<string> { "I", "A" }, Morse.Possibilities(".?"));
+            CollectionAssert.AreEqual(new List<string> { "E", "T" }, Morse.Possibilities("?"));
+            CollectionAssert.AreEqual(new List<string> { "I", "N" }, Morse.Possibilities("?."));
+            CollectionAssert.AreEqual(new List<string> { "I", "A" }, Morse.Possibilities(".?"));
         }
     }
 }
