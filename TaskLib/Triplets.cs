03ab261b2c8dc9fcb7a9aa83c31049feeb28fb2c
Alexandre Konioukhov <alexan1@gmail.com>
2019-12-02 20:01:20 -0500

    triplets

---
+++ TaskLib/Triplets.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace TaskLib
+{
+    public class Triplets
+    {
+        public IList<IList<int>> ThreeSum(int[] nums)
+        {
+
+            nums = nums.OrderBy(x => x).ToArray();
+            var n = nums.Length;
+            IList<IList<int>> result = new List<IList<int>>();
+            for (int i = 0; i < n - 2; i++)
+            {
+                for (int j = i + 1; j < n - 1; j++)
+                {
+                    for (int k = j + 1; k < n; k++)
+                    {
+                        if (nums[i] + nums[j] + nums[k] == 0)
+                        {
+                            var re = new List<int>()
+                        {nums[i], nums[j], nums[k]};
+                            //if (!result.Exists(x => x == nums))
+                            //if (!result.Any(item => item == re))
+                            if (!result.Contains(re))
+                                result.Add(re);
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ UnitTestLib/TripletsTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TaskLib;
+
+namespace UnitTestLib
+{
+
+    [TestClass]
+    public class TripletsTest
+    {
+        [TestMethod]
+        public void TripletsTest1()
+        {
+            int[] input = { -1, 0, 1, 2, -1, -4 };
+            var expected = new List<IList<int>>() { { -1, -1, 2 }, { -1, 0, 1 } };
+
+            CollectionAssert.AreEqual(18, Sum.MaxSumDivThree(nums));
+        }
+        [TestMethod]
+        public void SumTest2()
+        {
+            int[] nums = {4};
+
+            Assert.AreEqual(0, Sum.MaxSumDivThree(nums));
+        }
+        [TestMethod]
+        public void SumTest3()
+        {
+            int[] nums = { 1, 2, 3, 4, 4 };
+
+            Assert.AreEqual(12, Sum.MaxSumDivThree(nums));
+        }
+    }
+}
