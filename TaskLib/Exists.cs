8a51bb99ca4f229f8ddb48a185779b8ec789ec3e
Alexandre Konioukhov <alexan1@gmail.com>
2019-12-18 23:21:40 -0500

    exists

---
+++ TaskLib/Exists.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskLib
+{
+    public static class Exists
+    {
+        public static bool Answer(int[] ints, int k)
+        {
+            return Array.BinarySearch(ints, k) >= 0;
+        }
+    }
+}
---
+++ UnitTestLib/ExistsTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TaskLib;
+
+namespace UnitTestLib
+{
+
+    [TestClass]
+    public class ExistsTests
+    {
+        [TestMethod]
+        public void ExistsTest1()
+        {
+
+            int[] ints = { -9, 14, 37, 102 };
+            var num = 102;            
+
+            Assert.IsTrue(Exists.Answer(ints, num));
+        }
+        [TestMethod]
+        public void ExistsTest2()
+        {
+
+            int[] ints = { -9, 14, 37, 102 };
+            var num = 36;
+
+            Assert.IsFalse(Exists.Answer(ints, num));
+        }        
+    }
+}
