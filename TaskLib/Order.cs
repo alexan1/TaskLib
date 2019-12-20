5e84d281f6162e79095d51c2c49da2b8e7edda93
Alexandre Konioukhov <alexan1@gmail.com>
2019-12-20 10:52:19 -0500

    Order

---
+++ TaskLib/Order.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TaskLib
+{
+    public class Order
+    {
+        public string Customer { get; set; }
+        public decimal Price { get; set; }
+
+        public static IEnumerable<string> GetSuperCustomers(List<Order> orders)
+        {
+            return orders.Where(x => x.Price > 100).Select(x => x.Customer);
+        }
+
+    }
+
+    
+}
---
+++ UnitTestLib/OrderTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using TaskLib;
+
+namespace UnitTestLib
+{
+
+    [TestClass]
+    public class OrderTests
+    {
+        [TestMethod]
+        public void OrderTest1()
+        {
+
+            var orders = new List<Order>
+            {
+                new Order { Customer = "C3", Price = 125m },
+                new Order { Customer = "C2", Price = 150m },
+                new Order { Customer = "C1", Price = 50m },
+                new Order { Customer = "C1", Price = 25m }
+            };
+
+            var expected = new List<string> { "C3", "C2" };
+            
+            CollectionAssert.AreEqual(expected, Order.GetSuperCustomers(orders).ToList());
+        }
+
+    }
+}
