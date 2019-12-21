06e824244a13f4b50767378eac385801141f3438
Alexandre Konioukhov <alexan1@gmail.com>
2019-12-21 12:51:55 -0500

    filter1

---
+++ TaskLib/Filter1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TaskLib
+{
+    public static class Filter1
+    {
+        public static IEnumerable<string> Filter(List<string> strings)
+        {
+            return strings.Where(x => x.StartsWith("L")).OrderBy(x => x);
+        }
+    }
+}
---
+++ UnitTestLib/Filter1Tests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using TaskLib;
+
+namespace UnitTestLib
+{
+
+    [TestClass]
+    public class Filter1Tests
+    {
+        [TestMethod]
+        public void Filter1Test1()
+        {
+            var strings = new List<string>
+            {
+                "Gurt",
+                "Lobster",
+                "Litch",
+                "Doe"
+            };
+            var filteredStrings = Filter1.Filter(strings).ToList();
+
+            var expected = new List<string> { "Litch", "Lobster" };
+
+            CollectionAssert.AreEqual(expected, filteredStrings);
+        }
+    }
+}
