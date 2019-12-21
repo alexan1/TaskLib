ce00ee42dd1a4969d021e094349413414fb3b8f5
Alexandre Konioukhov <alexan1@gmail.com>
2019-12-21 14:34:16 -0500

    Counter

---
+++ TaskLib/Counter.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading;
+
+namespace TaskLib
+{
+    public class Counter
+    {
+        private static int count = 0;
+
+        public static int Increment()
+        {            
+            return Interlocked.Increment(ref count);
+        }
+    }
+}
