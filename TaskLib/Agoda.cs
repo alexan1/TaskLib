b9e6b50874327c124379f9d9beb65556eee549d8
Alexandre Konioukhov <alexan1@gmail.com>
2020-02-13 12:25:00 -0500

    agoda

---
+++ TaskLib/Agoda.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TaskLib
+{
+    public static class Agoba
+    {
+        //Sort a list of dates in ascending order (from past to pesent)
+        public static List<string> sortDates(List<string> dates)
+        {
+
+            return dates.Select(date => DateTime.Parse(date)).ToList().OrderBy(date => date).Select(date => date.ToString()).ToList();
+
+
+        }
+
+        //Given an array of integers for example: [5, 23, 23, 66, 5, 23, 7, 0]
+        //Find the two elements without a pair (one time in the array)
+        //and return an array form for example: [66, 7]
+        public static List<int> getFirstTwoElementsWithoutPair(List<int> list)
+        {
+            return list.GroupBy(i => i)
+                    .Where(g => g.Count() % 2 == 1)
+                    .Select(g => g.Key).ToList();
+        }
+
+        //Write a function that takes a string of words and returns then in reverse order (separated by sigle sapces).
+        //Example: "agoda best apps" will return "apps best agoda"
+        public static string reverseWords(string inputWords)
+        {            
+            var words = inputWords.Split(default(char[]), StringSplitOptions.RemoveEmptyEntries);
+            Array.Reverse(words);
+             return string.Join(" ", words);
+        }
+    }
+}
---
+++ UnitTestLib/AgodaTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using TaskLib;
+
+namespace UnitTestLib
+{
+
+    [TestClass]
+    public class AgodaTests
+    {
+        [TestMethod]
+        public void ReverseTest()
+        {
+            var input = "agoda best apps";
+            var expected = "apps best agoda";
+
+
+            Assert.AreEqual(expected, Agoba.reverseWords(input));
+        }
+        [TestMethod]
+        public void WithoutPairTest()
+        {
+            var input = new List<int> { 5, 23, 23, 66, 5, 23, 7, 0 };
+            var expected = new List<int> { 66, 7 };
+
+
+            CollectionAssert.AreEqual(expected, Agoba.getFirstTwoElementsWithoutPair(input));
+        }
+    }
+}
