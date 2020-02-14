5dcfb5056e9a5d80198bbb6afe3b153e2c5e10b5
Alexandre Konioukhov <alexan1@gmail.com>
2020-02-14 09:52:15 -0500

    SortDates fix

--- TaskLib/Agoda.cs
+++ TaskLib/Agoda.cs
@@ -8,27 +8,24 @@ namespace TaskLib
     public static class Agoba
     {
         //Sort a list of dates in ascending order (from past to pesent)
-        public static List<string> sortDates(List<string> dates)
+        public static List<string> SortDates(List<string> dates)
         {
-
-            return dates.Select(date => DateTime.Parse(date)).ToList().OrderBy(date => date).Select(date => date.ToString()).ToList();
-
-
+            return dates.Select(date => DateTime.Parse(date)).ToList().OrderBy(date => date).Select(date => date.ToString("dd MMM yyyy")).ToList();
         }
 
         //Given an array of integers for example: [5, 23, 23, 66, 5, 23, 7, 0]
         //Find the two elements without a pair (one time in the array)
         //and return an array form for example: [66, 7]
-        public static List<int> getFirstTwoElementsWithoutPair(List<int> list)
+        public static List<int> GetFirstTwoElementsWithoutPair(List<int> list)
         {
             return list.GroupBy(i => i)
-                    .Where(g => g.Count() % 2 == 1)
-                    .Select(g => g.Key).ToList();
+                    .Where(g => g.Count() == 1)
+                    .Select(g => g.Key).Take(2).ToList();
         }
 
         //Write a function that takes a string of words and returns then in reverse order (separated by sigle sapces).
         //Example: "agoda best apps" will return "apps best agoda"
-        public static string reverseWords(string inputWords)
+        public static string ReverseWords(string inputWords)
         {            
             var words = inputWords.Split(default(char[]), StringSplitOptions.RemoveEmptyEntries);
             Array.Reverse(words);
--- UnitTestLib/AgodaTest.cs
+++ UnitTestLib/AgodaTest.cs
@@ -15,7 +15,7 @@ namespace UnitTestLib
             var expected = "apps best agoda";
 
 
-            Assert.AreEqual(expected, Agoba.reverseWords(input));
+            Assert.AreEqual(expected, Agoba.ReverseWords(input));
         }
         [TestMethod]
         public void WithoutPairTest()
@@ -24,7 +24,19 @@ namespace UnitTestLib
             var expected = new List<int> { 66, 7 };
 
 
-            CollectionAssert.AreEqual(expected, Agoba.getFirstTwoElementsWithoutPair(input));
+            CollectionAssert.AreEqual(expected, Agoba.GetFirstTwoElementsWithoutPair(input));
+        }
+
+        [TestMethod]
+        public void SortDatesTest()
+        {
+            var input = new List<string> {"20 Oct 2052", "06 Jun 1933", "26 May 1960", "20 Sep 1958",
+                "16 Mar 2068", "25 May 1912", "16 Dec 2018", "26 Dec 2061", "04 Nov 2030", "28 Jul 1963"};
+            var expected = new List<string> {"25 May 1912", "06 Jun 1933", "20 Sep 1958", "26 May 1960",
+                "28 Jul 1963", "16 Dec 2018", "04 Nov 2030", "20 Oct 2052", "26 Dec 2061", "16 Mar 2068"};
+
+
+            CollectionAssert.AreEqual(expected, Agoba.SortDates(input));
         }
     }
 }
