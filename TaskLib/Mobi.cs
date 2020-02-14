a4701eb627a25ac1036573c6f28a6a6632e8530a
Alexandre Konioukhov <alexan1@gmail.com>
2020-02-13 23:49:04 -0500

    MostCommonWord

---
+++ TaskLib/Mobi.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+namespace TaskLib
+{
+    public static class Mobi
+    {
+        public static string MostCommonWord(string text)
+        {
+            return text.Split(' ').GroupBy(x => x).Select(x => new { KeyField = x.Key, Count = x.Count() })
+            .OrderByDescending(x => x.Count).First().KeyField.ToString();
+        }
+    }
+}
---
+++ UnitTestLib/MobiTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using TaskLib;
+
+namespace UnitTestLib
+{
+    [TestClass]
+    public class MobiTest
+    {
+        [TestMethod]
+        public void MostCommonWordTest()
+        {
+            var words = "two one three one three one";
+            var expected = "one";
+            Assert.AreEqual(expected, Mobi.MostCommonWord(words));
+        }
+    }
+}
