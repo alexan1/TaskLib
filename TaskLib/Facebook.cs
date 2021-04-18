aaa2d9ba895e3de9cacb7fb7213ab48178bc20b1
Alexandre Konioukhov <alexan1@gmail.com>
2021-04-18 13:40:24 -0400

    facebook

--- TaskLib/Facebook.cs
+++ TaskLib/Facebook.cs
@@ -3,22 +3,32 @@ using System.Text;
 
 namespace TaskLib
 {
-    public class RotationalCipher
+    public static class Facebook
     {
+        public static int NumberOfWays(int[] arr, int k)
+        {
+            int count = 0; 
+                        
+            for (int i = 0; i < arr.Length; i++)
+                for (int j = i + 1; j < arr.Length; j++)
+                    if ((arr[i] + arr[j]) == k)
+                        count++;
+            return count;
+        }
 
-        public static string rotationalCipher(String input, int rotationFactor)
+        public static string RotationalCipher(String input, int rotationFactor)
         {
             StringBuilder result = new StringBuilder();
-                       
-            foreach(var ch in input)
+
+            foreach (var ch in input)
             {
                 if (char.IsDigit(ch))
-                {                  
+                {
                     var ch1 = ((char.GetNumericValue(ch) + rotationFactor) % 10).ToString();
                     result.Append(ch1);
                 }
                 else if (char.IsLetter(ch))
-                {                    
+                {
 
                     if (char.IsUpper(ch))
                     {
@@ -40,5 +50,13 @@ namespace TaskLib
             }
             return result.ToString();
         }
+
+        private static int[] CountSubarrays(int[] arr)
+        {
+            
+            return new int[0];
+        }
     }
 }
+}
+
---
+++ UnitTestLib/FacebookTest.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TaskLib;
+
+namespace UnitTestLib
+{
+
+    [TestClass]
+    public class FacebookTest
+    {
+        [TestMethod]
+        public void PairSumTest1()
+        {
+            int[] nums = {1,2,3,4,3};
+            var sum = 6;
+            var result = 2;
+
+            Assert.AreEqual(result, Facebook.NumberOfWays(nums, sum));
+        }
+        [TestMethod]
+        public void PairSumTest2()
+        {
+            int[] nums = {1,5,3,3,3};
+            var sum = 6;
+            var result = 4;
+
+            Assert.AreEqual(result, Facebook.NumberOfWays(nums, sum));
+        }
+
+        [TestMethod]
+        public void CipherTest1()
+        {
+            var input = "Zebra-493?";
+            var rotationFactor = 3;
+            var output = "Cheud-726?";
+
+            Assert.AreEqual(output, Facebook.RotationalCipher(input, rotationFactor));
+        }
+        [TestMethod]
+        public void CipherTest2()
+        {
+            var input = "abcdefghijklmNOPQRSTUVWXYZ0123456789";
+            var rotationFactor = 39;
+            var output = "nopqrstuvwxyzABCDEFGHIJKLM9012345678";
+
+            Assert.AreEqual(output, Facebook.RotationalCipher(input, rotationFactor));
+        }
+    }
+}
