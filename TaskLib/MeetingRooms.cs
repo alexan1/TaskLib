5062f08b781e196ec5b14613e0a0e4e3fdc15d74
Alexandre Konioukhov <alexan1@gmail.com>
2021-04-22 14:35:17 -0400

    MeetingRooms

---
+++ TaskLib/MeetingRooms.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace TaskLib
+{
+    public static class MeetingRooms
+    {
+        //Given an array of meeting time intervals consisting of start and end times[[s1, e1],[s2, e2],...] (si<ei),
+        //determine if a person could attend all meetings.
+        public static bool CanAttendMeeting(int[][] intervals)
+        {
+            var len = intervals.Length;
+            var startTime = new int[len];
+            var endTime = new int[len];
+            var count = 0;
+            foreach (var interval in intervals)
+            {
+                startTime[count] = interval[0];
+                endTime[count++] = interval[1];
+            }
+            Array.Sort(startTime);
+            Array.Sort(endTime);
+            for (var i = 1; i < len; i++)
+            {
+                if (startTime[i] < endTime[i - 1]) return false;
+            }
+            return true;
+        }
+
+        // Given an array of time intervals for people entering and leaving a room consisting of start and end times
+        // [[s1,e1],[s2,e2],...] (si < ei). What is the maximum number of people in the room simultaneously?
+        public static int MaxPeopleNumber(int[][] people)
+        {
+            var numberArray = new int[people.Length];
+            const int result = 1;
+            //foreach (var sec in people[][1])
+            //foreach (var fs in people[0])
+            //{
+            //    if (sec < fs)
+            //        number++;
+            //}
+
+
+            return result;
+        }
+    }
+}
---
+++ UnitTestLib/MeetingRoomsTest.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TaskLib;
+
+namespace UnitTestLib
+{
+
+    [TestClass]
+    public class MeetingRoomsTest
+    {
+        [TestMethod]
+        public void CanAttendMeetingTest1()
+        {
+            var input = new[]
+            {
+                new int[] {0, 30},
+                new int[] {5, 10},
+                new int[] {15, 20}};
+
+            const bool result = false;
+
+            Assert.AreEqual(result, MeetingRooms.CanAttendMeeting(input));
+        }
+
+        [TestMethod]
+        public void CanAttendMeetingTest2()
+        {
+            var input = new[]
+            {
+                new int[] {17, 10},
+                new int[] {2, 4}};
+
+            const bool result = true;
+
+            Assert.AreEqual(result, MeetingRooms.CanAttendMeeting(input));
+        }
+
+        [TestMethod]
+        public void CanAttendMeetingTest3()
+        {
+            var input = new[]
+            {
+                new int[] {2, 10},
+                new int[] {3, 4},
+                new int[] {6, 9}};
+
+            const bool result = false;
+
+            Assert.AreEqual(result, MeetingRooms.CanAttendMeeting(input));
+        }
+
+        [TestMethod]
+        public void MaxPeopleNumberTest1()
+        {
+            var input = new[]
+            {
+                new int[] {2, 10},
+                new int[] {3, 4},
+                new int[] {6, 9}
+            };
+
+            const int result = 2;
+
+            Assert.AreEqual(result, MeetingRooms.MaxPeopleNumber(input));
+        }
+
+        [TestMethod]
+        public void MaxPeopleNumberTest2()
+        {
+            var input = new[]
+            {
+                new int[] {1, 3},
+                new int[] {2, 4},
+                new int[] {2, 5},
+                new int[] {3, 4},
+                new int[] {3, 6},
+                new int[] {4, 5},
+                new int[] {5, 6}
+            };
+
+            const int result = 4;
+
+            Assert.AreEqual(result, MeetingRooms.MaxPeopleNumber(input));
+        }
+
+        [TestMethod]
+        public void MaxPeopleNumberTest3()
+        {
+            var input = new[]
+            {
+                new int[] {1, 8},
+                new int[] {2, 7},
+                new int[] {3, 5},
+                new int[] {5, 7},
+                new int[] {6, 7},
+                new int[] {6, 8},
+                new int[] {7, 8},
+            };
+
+            const int result = 5;
+
+            Assert.AreEqual(result, MeetingRooms.MaxPeopleNumber(input));
+        }
+
+    }
+}
