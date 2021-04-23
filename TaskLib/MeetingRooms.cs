16775b02a49f88d88450faba010f43e9e0777050
Alexandre Konioukhov <alexan1@gmail.com>
2021-04-23 14:29:57 -0400

    MaxPeopleNumber

--- TaskLib/MeetingRooms.cs
+++ TaskLib/MeetingRooms.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace TaskLib
 {
@@ -30,14 +32,18 @@ namespace TaskLib
         // [[s1,e1],[s2,e2],...] (si < ei). What is the maximum number of people in the room simultaneously?
         public static int MaxPeopleNumber(int[][] people)
         {
-            var numberArray = new int[people.Length];
-            const int result = 1;
-            //foreach (var sec in people[][1])
-            //foreach (var fs in people[0])
-            //{
-            //    if (sec < fs)
-            //        number++;
-            //}
+            var all = new List<int>();
+
+            foreach (var man in people)
+            {
+                var rec = Enumerable.Range(man[0], man[1] - man[0]);
+                all.AddRange(rec);
+            }
+
+            var result = all.GroupBy(s => s)
+                .OrderByDescending(s => s.Count())
+                .Select(s => s.Count()).First();
+
 
 
             return result;
